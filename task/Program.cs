d0032c145efbe884e179024fde2bc28cc33479ac
Vadim <mihailovvadick1996@yandex.ru>
2022-12-02 14:10:07 +0300

    delete empty str

--- task/Program.cs
+++ task/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
-// Пример: ["hello","-2","world","=)"] -> [-2,"=)"]
+// Пример: ["hello","-2","world","=)"] -> [-2,":-)"]
 
-string[] array = { "hello", "-2", "world", "=)" };
+string[] array = { "hello", "-2", "world", ":-)" };
 
 
 
@@ -9,14 +9,13 @@ string[] CountElementsOfArray(string[] array)
 {
     
     string[] newArray = new string[array.Length];
-
     for (int i = 0; i < array.Length; i++)
     {
         int count = array[i].Length;
         if (count <= 3)
         {
-            newArray[i] += array[i];
-        }
+            newArray[i] = array[i];    
+        } 
     }
     return newArray;
 }
@@ -26,7 +25,7 @@ void PrintArray(string[] array)
     Console.Write("[");
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write($"{array[i]} "); 
+        Console.Write($"{array[i]}");
         if (i < array.Length - 1) Console.Write(",");
     }
     Console.Write("]");
